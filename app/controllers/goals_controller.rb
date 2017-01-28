967f511c11f95a346070376629b040e44d806d62
sallycai979 <sallycai979@gmail.com>
2017-01-28 09:31:51 +0000

    Show goals

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,6 +1,11 @@
 class GoalsController < ApplicationController
 before_filter :logged_in_user, only: [:show,:create, :edit]
 
+def index
+ @goals = current_user.goals
+end
+
+
 def new
  @goal = current_user.goals.build if logged_in?
 end
@@ -9,7 +14,7 @@ end
     @goal = current_user.goals.build(goal_params)
     if @goal.save
       flash[:success] = "Your goal has been created!"
-      redirect_to root_url
+      redirect_to goals_url
     else
       render user_url
     end
